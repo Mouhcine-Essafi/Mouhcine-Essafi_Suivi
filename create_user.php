b1e4296d253be4f270abe1a587fb35aed8519655
Anas-Diabi <33428345+Hackermans007@users.noreply.github.com>
2023-12-26 11:47:53 +0100

    create_user form.

--- create_user.php
+++ create_user.php
@@ -1,2 +1,20 @@
-<?php
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create User</title>
+</head>
+<body>
+    <h2>Create User</h2>
+    <form method="post" action="">
+        <label for="username">Username:</label>
+        <input type="text" name="username" required>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" required>
+
+        <button type="submit">Add User</button>
+    </form>
+</body>
+</html>
