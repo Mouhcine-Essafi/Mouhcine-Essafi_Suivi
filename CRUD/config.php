0f5b771cec8b39c495cd0f23f9d9663ee3b3b44b
Anas-Diabi <33428345+Hackermans007@users.noreply.github.com>
2024-01-02 09:22:19 +0100

    database

--- CRUD/config.php
+++ CRUD/config.php
@@ -3,7 +3,7 @@
 $host = "127.0.0.1";
 $username = "root";
 $password = "";
-$database = "php_training";
+$database = "anas_php_training";
 
 // Create a connection
 $conn = new mysqli($host, $username, $password, $database);
